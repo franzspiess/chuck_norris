7661d3c267a2b650527be0597ede38888068abf4
franzspiess <picnhepanchopincho@gmail.com>
2020-08-08 21:56:55 +0200

    Updates

---
+++ precache-manifest.7164270664d0566b83189a3d568188f3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5852425c3c7de5e6613ca5e5c3e8b2f",
+    "url": "/chuck_norris/index.html"
+  },
+  {
+    "revision": "e23bdcffbb2231a7b070",
+    "url": "/chuck_norris/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "a08c2485fbbae6bdee9d",
+    "url": "/chuck_norris/static/css/main.cf7ba21c.chunk.css"
+  },
+  {
+    "revision": "e23bdcffbb2231a7b070",
+    "url": "/chuck_norris/static/js/2.ce957bf7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chuck_norris/static/js/2.ce957bf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a08c2485fbbae6bdee9d",
+    "url": "/chuck_norris/static/js/main.b42dbc96.chunk.js"
+  },
+  {
+    "revision": "474a9a927ff37de9cc11",
+    "url": "/chuck_norris/static/js/runtime-main.b2c85f06.js"
+  },
+  {
+    "revision": "b92cd3e1cf52ab798e97724b4245b89f",
+    "url": "/chuck_norris/static/media/1118491.b92cd3e1.jpg"
+  }
+]);
